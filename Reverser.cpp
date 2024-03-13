1446314087046f069b9b099474a02386a4f16aa6
Angus-Stoba <angus.stoba@student.adelaide.edu.au>
2024-03-13 11:24:22 +1030

    new file:   Reverser.cpp 	new file:   Reverser.h

---
+++ Reverser.cpp
@@ -0,0 +1,22 @@
+// Defines recurring functions for Reverser
+
+#include "Reverser.h"
+
+int Reverser::reverseDigit(int value){
+    if (value < 10){
+        return value;
+    }
+    else {
+        
+    }
+}
+
+
+string Reverser::reverseString(string value){
+    if (){
+    
+    }
+    else {
+        
+    }
+}
---
+++ Reverser.h
@@ -0,0 +1,19 @@
+// Reverser takes string / int variables 
+
+#ifndef REVERSER_H
+#define REVERSER_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class Reverser {
+public:
+    int reverseDigit(int value);
+    string reverseString(string characters);
+};
+
+#endif
