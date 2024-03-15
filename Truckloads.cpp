976af1b5c95d6cdc5a96b0646e6188deb2a3ad27
Angus-Stoba <angus.stoba@student.adelaide.edu.au>
2024-03-15 23:33:59 +1030

    modified:   Truckloads.cpp

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -7,9 +7,9 @@ int Truckloads::numTrucks(int numCrates, int loadSize){
         return 1; // when the numCrates is <= loadSize, that 'pile' only needs 1 truck
     }
     else {
-        int a = numCrates / 2; // 'a' is the smaller half (in terms of odd numbers)
-        int b = numCrates - a; // 'b' is the larger half
-        return (numTrucks(a, loadSize) + numTrucks(b, loadSize));
+        int half1 = numCrates / 2; // 'a' is the smaller half (in terms of odd numbers)
+        int half2 = numCrates - half1; // 'b' is the larger half
+        return (numTrucks(half1, loadSize) + numTrucks(half2, loadSize));
         // each half is recursive for as long as they are larger that loadsize
     }
 }
