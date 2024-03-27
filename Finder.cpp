c0a89b7ad4419de875dfefc8a4cb9da6ab7fdca9
Angus-Stoba <angus.stoba@student.adelaide.edu.au>
2024-03-27 11:40:16 +1030

    modified:   Finder.cpp

--- Finder.cpp
+++ Finder.cpp
@@ -1,4 +1,22 @@
 #include "Finder.h"
+using namespace std;
+vector<int> Finder::findSubstrings(string s1, string s2) {
+vector<int> result;
+for(size_t i = 1; i <= s2.size(); i++) {
+size_t found = s1.find(s2.substr(0, i));
+if (found != string::npos) {
+result.push_back(found);
+} else {
+result.push_back(-1);
+}
+}
+return result;
+}
+
+
+
+/*
+#include "Finder.h"
 
 using namespace std;
 
@@ -25,3 +43,4 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
     }
 return result;
 }
+*/
